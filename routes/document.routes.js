b3b18ebffe8fd73d7cd0f9085b07864e5b9f9fac
krishmungase <mungasekrishna8@gmail.com>
2025-07-04 01:05:56 +0530

    feat: add JWT authentication middleware to document creation route

--- routes/document.routes.js
+++ routes/document.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { DocumentController } from "../controllers/document.controller.js";
+import authenticateJWT from "../middleware/authenticateJwt.js";
 const documentRoutes = express.Router();
 
 const documentController = new DocumentController();
@@ -8,5 +9,11 @@ documentRoutes.get(
   '/',
   (req, res, next) => documentController.getDocuments(req, res, next)
 );
+
+documentRoutes.post(
+  '/',
+  authenticateJWT,
+  (req, res, next) => documentController.createDocument(req, res, next)
+)
 
 export default documentRoutes;
