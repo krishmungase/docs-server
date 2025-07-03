9e434f1c002b2ce992278de242ab029680d36d6f
krishmungase <mungasekrishna8@gmail.com>
2025-07-04 01:06:01 +0530

    feat: implement user authentication in document creation process

--- controllers/document.controller.js
+++ controllers/document.controller.js
@@ -1,5 +1,7 @@
+import createHttpError from "http-errors";
 import logger from "../config/logger.js";
 import {
+  AuthService,
   DocumentService
 } from "../service/index.js"
 
@@ -7,10 +9,23 @@ export class DocumentController {
   constructor() {
     this.logger = logger
     this.documentService = new DocumentService()
+    this.authService = new AuthService()
   }
 
   async getDocuments(req, res, next) {
     const documents = await this.documentService.getAllDocuments()
     return res.json({ message: { documents } });
+  }
+
+  async createDocument(req, res, next) {
+    const document = req.body;
+    const userId = req.userId;
+
+    const user = await this.authService.findUserById(userId);
+    if (!user) return next(createHttpError(401, "Unauthorized"));
+
+    const newDocument = await this.documentService.createDocument(document, userId);
+
+    return res.json({ message: { document: newDocument } });
   }
 }
